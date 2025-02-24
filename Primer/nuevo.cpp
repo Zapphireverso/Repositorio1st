983366a6058c3035ae85bdcc6ede193acef4df31
Zapphire <nicolaspaiva1024@gmail.com>
2025-02-24 16:33:37 -0300

    reparando (errores)

--- Primer/nuevo.cpp
+++ Primer/nuevo.cpp
@@ -4,11 +4,7 @@ using namespace std;
 
 int main (){
 
-<<<<<<< HEAD
-    int autos = 556;
-=======
     int autos = 5555;
->>>>>>> ramaB
 
     cout << "Autos estacionados " << autos << endl;
     cin >> autos;
